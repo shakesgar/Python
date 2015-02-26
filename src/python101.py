74b88bd6ea9d7873d784e2660f8bd90bf9dc6a36
Gareth Burke <shakesgar@gmail.com>
2015-02-26 02:41:27 +0000

    Python added

--- src/python101.py
+++ src/python101.py
@@ -1,9 +1,14 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+from bs4 import BeautifulSoup
+import urllib
 
-__author__ = "Protools"
-__date__ = "$24-Feb-2015 22:11:06$"
 
-if __name__ == "__main__":
-    print "Hello World"
+response = urllib.urlopen("http://www.google.com")
+doc = response.read()
+
+soup = BeautifulSoup(doc)
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
